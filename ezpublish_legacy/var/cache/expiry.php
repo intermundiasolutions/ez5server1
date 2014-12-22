0d24e583f2ab6252b2fcec72bb00f2e4fe7034e3
asgit <andreas@intermundia.de>
2014-12-22 14:30:22 +0100

    - eZ Publish Community Project 2014.11

---
+++ ezpublish_legacy/var/cache/expiry.php
@@ -0,0 +1,12 @@
+<?php
+$Timestamps = array (
+  'content-view-cache' => 1416144994,
+  'class-identifier-cache' => 1416144995,
+  'image-manager-alias' => 1416144994,
+  'global-template-block-cache' => 1416144994,
+  'user-info-cache' => 1416144994,
+  'content-tree-menu' => 1416144994,
+  'active-extensions-cache' => 1416144994,
+  'ts-translation-cache' => 1416144994,
+);
+?>
---
+++ ezpublish_legacy/var/cache/ini/file-943a546a9c04d44eab070515c866079c.php
@@ -0,0 +1,12 @@
+<?php
+// This is a auto generated ini cache file, time created:Sun, 16 Nov 14 14:36:34 +0100
+$data = array(
+'rev' => 2,
+'created' => '2014-11-16T14:36:34+01:00',
+'charset' => "utf-8",
+'files' => array (0 => '/mntraid/jenkins.std/jobs/ezpublish5-community/workspace/build/ezpublish5/ezpublish_legacy/lib/ezutils/classes/../../../settings/file.ini',
+),
+'file' => '/mntraid/jenkins.std/jobs/ezpublish5-community/workspace/build/ezpublish5/ezpublish_legacy/lib/ezutils/classes/../../../settings/file.ini',
+'val' => array ('BinaryFileSettings' => array ('Handler' => 'eZFilePassthroughHandler',),'FileSettings' => array ('SystemUserCheck' => 'disabled','RequiredSystemUser' => 'www-data','Handlers' => array ('gzip' => 'ezgzipcompressionhandler','gzipzlib' => 'ezgzipzlibcompressionhandler','gzipshell' => 'ezgzipshellcompressionhandler',),),'ClusteringSettings' => array ('FileHandler' => 'eZFSFileHandler','NonExistantStaleCacheHandling' => array ('viewcache' => 'wait','cacheblock' => 'wait','misc' => 'wait',),),'eZDFSClusteringSettings' => array ('MountPointPath' => '','DBBackend' => 'eZDFSFileHandlerMySQLiBackend','DFSBackend' => 'eZDFSFileHandlerDFSBackend','DBHost' => 'dbhost','DBPort' => '3306','DBSocket' => '','DBName' => 'cluster','DBUser' => 'root','DBPassword' => '','DBConnectRetries' => '3','DBExecuteRetries' => '20','MaxCopyRetries' => '5',),'PassThroughSettings' => array ('DefaultContentDisposition' => 'attachment','ContentDisposition' => array ('application/x-shockwave-flash' => 'inline','video/ogg' => 'inline','video/mp4' => 'inline','video/webm' => 'inline',),),'ClusterEventsSettings' => array ('ClusterEvents' => 'disabled','Listener' => '',),
+));
+?>
